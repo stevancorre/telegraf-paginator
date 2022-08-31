f8096a8fc44854babde3e3cccd2840a52eb949ea
Stévan Corre <stevancorre@protonmail.com>
2022-09-01 01:06:22 +0200

    simple implementation with static data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,62 @@
-export default function Paginator(): void {
-    console.log("ok");
+import hash from "object-hash";
+import { Composer, Context } from "telegraf";
+import {
+    InlineKeyboardMarkup,
+    ParseMode,
+} from "telegraf/typings/core/types/typegram";
+
+export interface Page {
+    title: string;
+    data: string;
 }
+
+export class Paginator {
+    private readonly actionId: string;
+    private currentPage = 0;
+
+    public constructor(
+        private pages: ReadonlyArray<Page>,
+        private header?: string
+    ) {
+        this.actionId = hash(
+            hash(pages) + hash(header ?? Math.random().toString(64))
+        );
+    }
+
+    public text(): string {
+        return `${this.header ?? ""}\n${this.pages[this.currentPage].data}`;
+    }
+
+    public extra(): {
+        reply_markup: InlineKeyboardMarkup;
+        parseMode: ParseMode;
+    } {
+        return {
+            reply_markup: {
+                inline_keyboard: [
+                    this.pages.map((x: Page, i: number) => ({
+                        text: x.title,
+                        callback_data: this.callbackDataForPage(i),
+                    })),
+                ],
+            },
+            parseMode: "HTML",
+        };
+    }
+
+    private callbackDataForPage(page: number) {
+        return `${this.actionId}-${page}`;
+    }
+
+    public handleAction(bot: Composer<Context>): void {
+        bot.action(new RegExp(this.actionId + "-([0-9]+)"), async (ctx) => {
+            const newPage = parseInt(ctx.match[1]);
+            if (this.currentPage == newPage) return await ctx.answerCbQuery();
+            this.currentPage = newPage;
+
+            await ctx.editMessageText(this.text(), this.extra());
+        });
+    }
+}
+
+export { Context };
